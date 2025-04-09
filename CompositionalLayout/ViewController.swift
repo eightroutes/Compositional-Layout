2914f3f89e6d59a1eac6c0ac1346358635c0f909
EllGHT <rmsgh1188@gmail.com>
2025-04-09 17:57:53 +0900

    feat: categories 추가 #2

--- CompositionalLayout/ViewController.swift
+++ CompositionalLayout/ViewController.swift
@@ -18,34 +18,96 @@ class ViewController: UICollectionViewController {
     }
     
     static func createLayout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout { sectionIndex, environment in
+        return UICollectionViewCompositionalLayout {
+ sectionNumber,
+ environment in
             
-            let item = NSCollectionLayoutItem(
-                layoutSize: .init(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .fractionalHeight(1)
+            if sectionNumber == 0 {
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .fractionalHeight(1)
+                    )
                 )
-            )
-            item.contentInsets.trailing = 2
-            item.contentInsets.bottom = 16
-//            item.contentInsets.top = 16
-//            item.contentInsets.leading = 16
+                
+                item.contentInsets = NSDirectionalEdgeInsets(
+                    top: 0,
+                    leading: 0,
+                    bottom: 0,
+                    trailing: 2
+                )
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .absolute(200)
+                    ),
+                    subitems: [item]
+                )
+                
+                let section = NSCollectionLayoutSection(group: group)
+                
+                // 가로 배너
+                //            section.orthogonalScrollingBehavior = .continuous
+                section.orthogonalScrollingBehavior = .paging
+                return section
+                
+            } else {
+                
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(0.25),
+                        heightDimension: .absolute(150)
+                    )
+                )
+                
+                item.contentInsets = NSDirectionalEdgeInsets(
+                    top: 0,
+                    leading: 0,
+                    bottom: 16,
+                    trailing: 16
+                )
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .estimated(500)
+                    ),
+                    subitems: [item]
+                )
+//                group.contentInsets.leading = 16
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                
+                section.boundarySupplementaryItems = [
+                    .init(
+                        layoutSize: .init(
+                            widthDimension: .fractionalWidth(1),
+                            heightDimension: .absolute(50)
+                        ),
+                        elementKind: categoryHeaderId,
+                        alignment: .topLeading
+                    )
+                ]
+                
+                return section
+            }
             
-            let group = NSCollectionLayoutGroup.horizontal(
-                layoutSize: .init(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .absolute(200)
-                ),
-                subitems: [item]
-            )
-            let section = NSCollectionLayoutSection(group: group)
-            
-            // 가로 배너
-//            section.orthogonalScrollingBehavior = .continuous
-            section.orthogonalScrollingBehavior = .paging
-            return section
         }
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: headerId,
+            for: indexPath
+        )
+//        header.backgroundColor = .blue
+        return header
+    }
+    
+    let headerId = "headerId"
+    static let categoryHeaderId = "categoryHeaderId"
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 2
@@ -84,6 +146,34 @@ class ViewController: UICollectionViewController {
                 UICollectionViewCell.self,
                 forCellWithReuseIdentifier: cellId
             )
+        
+        collectionView
+            .register(
+                Header.self,
+                forSupplementaryViewOfKind: ViewController.categoryHeaderId,
+                withReuseIdentifier: headerId
+            )
+    }
+}
+
+class Header: UICollectionReusableView {
+    let label = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.text = "Categories"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        
+        addSubview(label)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
