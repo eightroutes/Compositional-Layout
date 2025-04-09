80af2f8956828f6a82e61404be794ace660f74df
EllGHT <rmsgh1188@gmail.com>
2025-04-09 15:01:11 +0900

    feat: 기본 레이아웃 구현

--- CompositionalLayout/ViewController.swift
+++ CompositionalLayout/ViewController.swift
@@ -11,12 +11,72 @@ import Then
 
 class ViewController: UICollectionViewController {
     
+    
+    init() {
+        let layout = ViewController.createLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, environment in
+            
+            let item = NSCollectionLayoutItem(
+                layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)
+                )
+            )
+            item.contentInsets.trailing = 16
+            item.contentInsets.bottom = 16
+            item.contentInsets.top = 16
+            item.contentInsets.leading = 16
+            
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(200)
+                ),
+                subitems: [item]
+            )
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: cellId,
+            for: indexPath
+        )
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    private let cellId = "cellID"
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        collectionView.backgroundColor = .black
+        navigationItem.title = "Food Delivery"
         
-        view.backgroundColor = .white
+        collectionView
+            .register(
+                UICollectionViewCell.self,
+                forCellWithReuseIdentifier: cellId
+            )
     }
+}
 
-
+#Preview {
+    ViewController()
 }
 
