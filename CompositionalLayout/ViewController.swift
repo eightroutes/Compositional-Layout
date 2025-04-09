91079ec71314db130e26d6805392dec77991d00c
EllGHT <rmsgh1188@gmail.com>
2025-04-09 14:24:20 +0900

    feat: first commit

---
+++ CompositionalLayout/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CompositionalLayout
+//
+//  Created by 정근호 on 4/9/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class ViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
